12a2287895d451c6a86737b576a1bbe24a201102
Greenie_T <greeniet87@gmail.com>
2021-03-07 00:35:44 +0000

    rules.yml working

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path"
+	"strings"
 	"time"
 )
 
@@ -36,7 +38,28 @@ func Logger(level string, message string) {
 	}
 }
 
+func askForConfirmation(s string) bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		response = strings.ToLower(strings.TrimSpace(response))
+
+		if response == "y" || response == "yes" {
+			return true
+		} else if response == "n" || response == "no" {
+			return false
+		}
+	}
+}
+
 func init() {
+	Logger("####", "##################")
+	Logger("INIT", "Starting AlertManager")
 	_, err := os.Stat(configpath)
 	if os.IsNotExist(err) {
 		fmt.Println("Config file missing")
@@ -47,51 +70,104 @@ func init() {
 		fmt.Println(basepath + " missing")
 		os.Exit(1)
 	}
-	fmt.Println("All good")
+	Logger("INIT", "setup up done.")
+	Logger("####", "##################")
 
 }
 
 func main() {
 	groups, _ := ioutil.ReadDir(basepath)
+	os.Rename("./rules.yml","./rules.bak")
+	// creating rules.yml 
+	r, err := os.OpenFile("./rules.yml", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer r.Close()
+	if _, err := r.WriteString("groups:\n"); err != nil {
+		log.Fatal(err)
+	}
 
 	for _, g := range groups {
 		if g.IsDir() {
-			fmt.Println("Group:" + g.Name())
+			Logger("INFO", "Groupname: "+g.Name())
 			alerts, _ := ioutil.ReadDir(path.Join(basepath, g.Name()))
+
+			f, err := os.OpenFile(path.Join(basepath, g.Name())+"/tmp_rules.yml", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			defer f.Close()
+			if _, err := f.WriteString("- name: " + g.Name() + "\n  rules:\n"); err != nil {
+				log.Fatal(err)
+			}
 
 			for _, a := range alerts {
 				if a.IsDir() {
-					fmt.Println(a.Name())
+					Logger("INFO", "Alertname: "+g.Name()+"/"+a.Name())
 					metapath := path.Join(basepath, g.Name(), a.Name()) + "/.meta.yml"
-					// var confDocID = meta.getConfDocID(metapath)
-					file, _ := os.Stat(metapath)
-					if meta.getModTime(metapath) < file.ModTime().Format("2006-01-02 15:04:05") {
-						Logger("WARN", "Manual changes of meta "+metapath+" file detected")
-						Logger("WARN", meta.getModTime(metapath)+" < "+file.ModTime().Format("2006-01-02 15:04:05"))
+					rulepath := path.Join(basepath, g.Name(), a.Name()) + "/rule.yaml"
 
+					// appending alertrules to tmp group file
+					if _, err := f.WriteString("  - alert: " + a.Name() + "\n"); err != nil {
+						log.Fatal(err)
+					}
+					rule, err := ioutil.ReadFile(rulepath)
+					if err != nil {
+						fmt.Println(err)
 						break
 					}
-					Logger("WARN", meta.getModTime(metapath)+" < "+file.ModTime().Format("2006-01-02 15:04:05"))
-					
-					if meta.getVersion(metapath) != conflunece.getVersionbyID(meta.getConfDocID(metapath)) {
+					if _, err := f.Write(rule); err != nil {
+						log.Fatal(err)
+					}
+					if _, err := f.WriteString("\n"); err != nil {
+						log.Fatal(err)
+					}
+
+					// checking for manual metafilechanges
+					file, err := os.Stat(metapath)
+					if err != nil {
+						log.Fatal(err)
+					}
+					if meta.getModTime(metapath) != "" && meta.getModTime(metapath) < file.ModTime().Format("2006-01-02 15:04") {
+						Logger("WARN", "ModTime: "+meta.getModTime(metapath)+" < OS_ModTime: "+file.ModTime().Format("2006-01-02 15:04"))
+						Logger("WARN", "Manual changes of meta "+metapath+" file detected")
+						Logger("WARN", "Changes of metafile should only be done by this tool. Manual changes can be done with the proper precautions.")
+						Logger("WARN", "Do you wanna proceed with this alert. No just skips this one. [y/n]")
+						ask := askForConfirmation("")
+						if !ask {
+							break
+						}
+					}
+
+					// update metafile
+					if meta.getVersion(metapath) != conflunece.getVersionbyID(meta.getConfDocID(metapath)) ||
+						meta.getAlertname(metapath) != a.Name() ||
+						meta.getAlertGroupName(metapath) != g.Name() {
+						Logger("INFO", "updating metafile")
 						meta.updateVersion(metapath, conflunece.getVersionbyID(meta.getConfDocID(metapath)))
-						meta.setModTime(metapath)
-					}
-					if meta.getAlertname(metapath) != a.Name() {
 						meta.setAlertName(metapath, a.Name())
-						meta.setModTime(metapath)
-					}
-					if meta.getAlertGroupName(metapath) != g.Name() {
 						meta.setAlertGroupName(metapath, g.Name())
-						meta.setModTime(metapath)
 					}
-					
-					fmt.Println(conflunece.getBodybyID(meta.getConfDocID(metapath)))
+
+					// Write conflunece json file
 					josnpath := path.Join(basepath, g.Name(), a.Name()) + "/.conflunece.json"
-					ioutil.WriteFile(josnpath, []byte(conflunece.getJsonbyID(meta.getConfDocID(metapath))), 0777)
-					
+					ioutil.WriteFile(josnpath, []byte(conflunece.getJsonbyID(meta.getConfDocID(metapath))), 0666)
 				}
 			}
+
+			// writing master rules file r
+			tmpgrprule, err := ioutil.ReadFile(path.Join(basepath, g.Name()) + "/tmp_rules.yml")
+			if err != nil {
+				fmt.Println(err)
+			}
+			if _, err := r.Write(tmpgrprule); err != nil {
+				log.Fatal(err)
+			}
+
+			// removing temp files
+			os.Remove(path.Join(basepath, g.Name()) + "/tmp_rules.yml")
+			Logger("INFO", "Group Done!")
 		}
 	}
 
